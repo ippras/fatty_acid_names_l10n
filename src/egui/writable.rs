8f14aed8f69036403e465c6e3b02ea08e6f90975
kgv <kgv@users.noreply.github.com>
2026-05-15 16:14:09 +0300

    3

--- src/egui/writable.rs
+++ src/egui/writable.rs
@@ -1,6 +1,9 @@
-use crate::egui::names::Names;
+use crate::egui::{ABBREVIATION, COMMON, IUPAC, names::Names};
 
+use const_format::formatcp;
 use egui::{InnerResponse, Response, Ui, Widget};
+use egui_l20n::UiExt;
+use egui_phosphor::regular::{ERASER, PENCIL};
 use typed_builder::TypedBuilder;
 
 /// Writable name widget
@@ -10,7 +13,7 @@ pub struct Writable<'a> {
     #[builder(default = true)]
     hover: bool,
     #[builder(default, setter(strip_option))]
-    text: Option<&'a str>,
+    text: Option<String>,
 }
 
 impl Writable<'_> {
@@ -18,6 +21,36 @@ impl Writable<'_> {
         let mut text = self.text.unwrap_or_default();
         let mut response = ui.text_edit_singleline(&mut text);
         if response.changed() {
+            return InnerResponse::new(Some(text.to_string()), response);
+        }
+        let mut changed = false;
+        response.context_menu(|ui| {
+            let id = self.id;
+            if ui.button((PENCIL, ABBREVIATION)).clicked()
+                && let Some(name) = ui.try_localize(&format!("{id}.abbreviation"))
+            {
+                text = name;
+                changed = true;
+            }
+            if ui.button((PENCIL, formatcp!("{COMMON} name"))).clicked()
+                && let Some(name) = ui.try_localize(&format!("{id}.common"))
+            {
+                text = name;
+                changed = true;
+            }
+            if ui.button((PENCIL, formatcp!("{IUPAC} name"))).clicked()
+                && let Some(name) = ui.try_localize(&format!("{id}.common"))
+            {
+                text = name;
+                changed = true;
+            }
+            if ui.button((ERASER, "Empty string")).clicked() {
+                text = String::new();
+                changed = true;
+            }
+        });
+        if changed {
+            response.mark_changed();
             return InnerResponse::new(Some(text.to_string()), response);
         }
         if self.hover {
